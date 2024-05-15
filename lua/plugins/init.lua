6c31480dd17be9bdc419a36b2aa7374284fcb9c9
Jacob E. Tuz Poot <jetp79@proton.me>
2024-05-14 23:11:31 -0600

    feat: add todo-comments.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -219,4 +219,13 @@ return {
     end,
     ft = { "markdown" },
   },
+  {
+    "folke/todo-comments.nvim",
+    lazy = false,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
 }
